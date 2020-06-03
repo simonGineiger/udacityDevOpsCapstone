84684101acef59c4742869b00ece78f8f0567b3f
Simon Gineiger <researchbookmarks@gmail.com>
2020-06-03 17:22:15 +0200

    changed --password to --password-stdin in login bash script

--- pushcontainer.sh
+++ pushcontainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PW=$1
-docker login -u simongineiger -p $PW
+docker login -u simongineiger --password-stdin $PW
 dockerpath=simongineiger/udacity_devops_capstone
 docker tag xclapi $dockerpath
 docker push $dockerpath
