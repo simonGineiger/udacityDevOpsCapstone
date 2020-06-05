7f6690831b6da9b930cb9279cab2f9538f5caf33
Simon Gineiger <researchbookmarks@gmail.com>
2020-06-05 15:50:15 +0200

    update kubeconfig in deployment step

--- deploycontainers.sh
+++ deploycontainers.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+aws eks update-kubeconfig --name UdacityDevOpsCapstoneK8S-Cluster
 /home/ubuntu/bin/kubectl apply -f k8s/api_deployment.yml
--- testawscreds.sh
+++ testawscreds.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 aws ec2 describe-instances
-# aws eks update-kubeconfig --name UdacityDevOpsCapstoneK8S-Cluster
 
 
